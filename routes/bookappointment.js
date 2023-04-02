ae06f78801e272c22b598116b2a65fa948c17725
shreyaskapoor22 <shreyas.kapoor@colorado.edu>
2023-04-01 19:41:57 -0600

    add cancel and get appointment functionalities

--- routes/bookappointment.js
+++ routes/bookappointment.js
@@ -21,16 +21,49 @@ module.exports = (app) => {
       })
   });
 
-  //Get appointment Details by user
+  //Function to show get appointments page. This page shows the list of appointments made by the user
   app.post("/getappointment", (req, res) => {
     console.log("Inside get appointment");
     appointment.find({userName: req.body.userName}).then((result => {
-        console.log("Found appointments");
-        console.log(result);
-            res.status(200).send({appointments: result})  ;          
+        if(Object.keys(result).length >0){
+            console.log("Found appointments");
+            console.log(result);
+                res.status(200).send({appointments: result})  ;  
+        }else{
+            console.log("No appointments for userName: ", req.body.userName);
+            return res.status(201).send({message: "Appointment does not exist"});
+        }
+        
     })).catch((error =>{
         console.log(error);
         res.status(404).send(error)
+    }))
+});
+
+//Cancel appointment made by user for a particular date, time, and propertyId
+app.post("/cancelappointment", (req, res) => {
+    console.log("Inside cancel appointment function");
+    appointment.findOne({userName: req.body.userName, 
+        appointmentDate: req.body.appointmentDate, 
+        appointmentTime: req.body.appointmentTime,
+        propertyId: req.body.propertyId}).then((result => {
+            if(result){
+                console.log("Appointment found");
+                appointment.deleteOne({_id: result._id}).then(function(){
+                    console.log("Data deleted"); // Success
+                    return res.status(200).send({message: "Data deleted"})
+                }).catch(function(error){
+                    console.log(error); // Failure
+                    return res.status(202).send({message: error})
+                });
+
+            }else{
+                return res.status(201).send({message: "Appointment does not exist"});
+            }
+                 
+    })).catch((error =>{
+        console.log(error);
+        return res.status(404).send(error)
     }))
 });
 };
