4028ec427b8769307d5f368ef107425c640557a6
shreyaskapoor22 <shreyas.kapoor@colorado.edu>
2023-04-07 19:24:52 -0600

    adding review and review analysis data

--- routes/property.js
+++ routes/property.js
@@ -1,17 +1,38 @@
 const mongoose = require("mongoose");
 const property = mongoose.model("propertyData");
+const review = mongoose.model("review");
+const reviewAnalysis = mongoose.model("reviewAnalysis");
 
 module.exports = (app) => {
   app.post("/property", (req, res) => {
-    //const { propertyId } = req.params.id;
     const { propertyId } = req.body;
     property
       .findOne({ propertyId: propertyId })
       .then((propertyResult) => {
-        console.log("Found property");
-        console.log(propertyResult);
         if (propertyResult) {
-          res.status(200).send({ property: propertyResult });
+          console.log(`Found property details for propertId: ${propertyResult.propertyId}`);
+          review.find(({propertyId: propertyResult.propertyId})).then((reviewResult =>{
+            if(Object.keys(reviewResult).length >0){
+              console.log(`Found ${Object.keys(reviewResult).length} reviews for ${propertyResult.propertyId}`);
+              reviewAnalysis.find({propertyId:propertyResult.propertyId}).then((reviewAnalysisResult =>{
+                if(reviewAnalysisResult){
+                  console.log(`Found reviewAnalysis for ${propertyResult.propertyId}`);
+                   return res.status(200).json({ property: propertyResult, reviews: reviewResult, reviewAnalysis: reviewAnalysisResult });
+                }else{
+                  console.log(`No reviewsAnalysis found for ${propertyResult.propertyId}`);
+                   res.status(200).json({ property: propertyResult, reviews: reviewResult, reviewAnalysis: {} });
+                }
+              }))
+              
+            }else{
+              console.log(`No reviews found for ${propertyResult.propertyId}`);
+               res.status(200).json({ property: propertyResult, reviews: {}, reviewAnalysis: {} });
+
+            }
+          }))
+        }else{
+          console.log(`No property Found for propertyId: ${propertyResult.propertyId}`)
+           res.status(200).send({ property: propertyResult });
         }
       })
       .catch((error) => {
